8c2f36e7535cad927e54fbbe8c516e21dc928a00
jzf21 <jozefneelamkavil2003@gmail.com>
2023-05-13 11:59:10 +0530

    contact card added

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,26 +1,38 @@
-// import React from 'react'
+import React from 'react'
+import contactimg from '../../assets/contact-idea.svg'
+const ContactUs = () => {
+  return (
+    <div id="contact" className="max-w-screen-xl py-5 mx-auto ">
+      <h2 className="  text-5xl text-center text-black font-semibold py-4  ">Contact Us </h2>
+      <div className="flex flex-col md:flex-row justify-evenly py-5 flex-wrap items-center gap-4">
+        <ContactCard
+          name="Mekha L"
+          designation={"Marketing Head"}
+          phone={"9542233441"}
+        />
+        <ContactCard
+          name="Mekha L"
+          designation={"Marketing Head"}
+          phone={"9542233441"}
+        />
+      </div>
+    </div>
+  );
+}
 
-// const ContactUs = () => {
-//   return (
-//     <div id="contact" className=''>
-//         <h2 className='text-3xl text-black'>Contact Us </h2>
-//         <div className='flex flex-col md:flex-row justify-center flex-wrap items-center gap-4'>
-//           <div className='flex flex-row'>
-//             <div
-//           </div>
-//         </div>
+export default ContactUs;
 
-//     </div>
-//   )
-// }
+const ContactCard = ({name,designation,phone}) => {
+  return (
+    <div className="flex flex-row justify-center items-center ">
+      <img src={contactimg} className="w-[100px] h-auto bg-white flex-start border border-black "/>
+      <div className="flex flex-col justify-center h-[100px]  p-4 min-w-[250px] border-r border-y border-black  ">
+        <h2 className='tracking-wider uppercase font-semibold'>{name}</h2>
+        <p>{designation}</p>
+        <p className='font-semibold text-xs mt-2'>{phone}</p>
+      </div>
+    </div>
+  );
+};
 
-// export default ContactUs
 
-
-// const ContactCard=()=>{
-//   return(
-//     <div>
-//     </div>
-
-//   )
-// }
