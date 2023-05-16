0473f0e8c537be96369045051f337e975ddbbd39
jzf21 <jozefneelamkavil2003@gmail.com>
2023-05-16 13:46:50 +0530

    more

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://debutsav-astro.github.io',
-  base: '/my-repo',
+  base: '/debutsav-astro',
 });
