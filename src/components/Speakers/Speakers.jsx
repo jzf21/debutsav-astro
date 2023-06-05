696d4f6ddbab14d697367c3887d0ae530a5455fb
jzf21 <jozefneelamkavil2003@gmail.com>
2023-06-05 20:05:58 +0530

    make changes to speaker

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,73 +1,36 @@
 import React from "react";
+import { speakersList } from "./data.jsx";
 
 const Speakers = () => {
-   const speakersList = [
-  {
-    name: "Andrew Batsin",
-    designation: "CTO, Hoppscotch",
-    img: "https://avatars.githubusercontent.com/u/9131943?v=4",
-    delay: 100
-  },
-  {
-    name: "Athul Cyriac Ajay",
-    designation: "Platform Engineer, Frappe",
-    img: "https://avatars.githubusercontent.com/u/40897573?v=4",
-    delay:200
-  },
-  {
-    name: "Abraham Raji",
-    designation: "Debian Developer",
-    img: "https://avatars.githubusercontent.com/u/32333507?v=4",
-    delay:300
-  },
-  {
-    name: "Sahil Dhiman ",
-    designation: "Debian Developer",
-    img: "https://seccdn.libravatar.org/avatar/fc0d3b6f6314f9f146ae50f07385f6a0?d=mm&s=96",
-    delay:400
-  },
-]
   return (
     <div id="speakers" className="bg-red text-white py-5 ">
       <h1 className="text-3xl text-center font-semibold p-3">Speakers</h1>
-      <div
-        className="flex flex-col md:flex-row justify-evenly"
-    
-      >
-        {speakersList.map(({ name, designation, img ,delay}) => (
-          <SpeakerCard {...{ name, designation, img ,delay}} />
+      <div className="flex flex-col md:flex-row justify-evenly flex-wrap">
+        {speakersList.map(({ name, designation, img, url }) => (
+          <SpeakerCard {...{ name, designation, img, url }} />
         ))}
       </div>
     </div>
   );
 };
 
-const SpeakerCard = ({name, designation, img,delay}) => {
+const SpeakerCard = ({ name, designation, img, url }) => {
   return (
-    <div
-      className="mt-6 w-fit mx-auto"
-      data-aos="zoom-in"
-      data-aos-duration="1100"
-      data-aos-delay={delay}
-      // data-aos="zoom-in"
-      // data-aos-duration="1000"
-    >
+    <a href={url} className="mt-6 w-fit mx-auto">
       <div className=" outline outline-white outline-8 rounded-full">
         <img
           src={img}
-          className=" rounded-full w-[200px] h-[200px] overflow-hidden "
-          alt="profile picture"
-          srcset=""
+          className=" rounded-full w-[200px] h-[200px] overflow-hidden object-cover"
+          alt={`${name}'s picture`}
         />
       </div>
-
       <div className="mt-8 ">
         <h2 className="text-white font-bold text-sm text-center tracking-wide">
           {name}
         </h2>
         <p className="text-center text-gray-400 text-xs mt-2">{designation}</p>
       </div>
-    </div>
+    </a>
   );
 };
 
---
+++ src/components/Speakers/data.jsx
@@ -0,0 +1,32 @@
+export const speakersList = [
+  {
+    name: "Andrew Batsin",
+    designation: "CTO, Hoppscotch",
+    img: "https://avatars.githubusercontent.com/u/9131943?v=4",
+    url: "https://andrewbast.in/",
+  },
+  {
+    name: "Athul Cyriac Ajay",
+    designation: "Platform Engineer, Frappe",
+    img: "https://avatars.githubusercontent.com/u/40897573?v=4",
+    url: "https://athulcyriac.in/",
+  },
+  {
+    name: "Sahil Dhiman",
+    designation: "Debian Developer",
+    img: "https://seccdn.libravatar.org/avatar/fc0d3b6f6314f9f146ae50f07385f6a0?d=mm&s=96",
+    url: "https://blog.sahilister.in/",
+  },
+  {
+    name: "Kurian Benoy",
+    designation: "Volunteer, SMC",
+    img: "https://kurianbenoy.com/posts/images/fossasia_summit_2019/my_lighting_talk.jpg",
+    url: "https://kurianbenoy.com",
+  },
+  {
+    name: "Abraham Raji",
+    designation: "Debian Developer",
+    img: "https://avatars.githubusercontent.com/u/32333507?v=4",
+    url: "https://abrahamraji.in",
+  },
+];
